d853834e1db8abe0b2d254ecf2d1190a23944052
KABUTOOOOOOOOOOO <bekoqwerty99@gmail.com>
2023-12-17 06:29:36 +0600

    customizing right menu

--- src/Components/RightMenu.js
+++ src/Components/RightMenu.js
@@ -1,8 +1,38 @@
 import React from "react";
 import "../Styles/RightMenu.css";
+import { FaCrown, FaBell, FaRegHeart, FaSun, FaCogs } from "react-icons/fa";
+import Profile from "../assets/profile.jpg";
 
 function RightMenu() {
-  return <div className="rightMenu"></div>;
+  return (
+    <div className="rightContainer">
+      <div className="goPro">
+        <i>
+          <FaCrown />
+          <p>
+            <span> Go</span>Premium
+          </p>
+        </i>
+
+        <i>
+          <FaBell />
+        </i>
+
+        <i>
+          <FaRegHeart />
+        </i>
+      </div>
+      <div className="profile">
+        <i>
+          <FaCogs />
+        </i>
+
+        <div className="profileImage">
+          <img src={Profile} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export { RightMenu };
