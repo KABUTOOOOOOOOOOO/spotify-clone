d9fff02956121960f6d0eb1a0e3961681c97a424
KABUTOOOOOOOOOOO <bekoqwerty99@gmail.com>
2023-12-19 22:37:27 +0600

    using spotify web api, customizing library page

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../Styles/Library.css";
 import { Playlists } from "../Components/PlayLists.js";
-import axios from "axios";
 
 const Library = () => {
-  const topPlaylists = Playlists.slice(0, 3);
-  const botPlaylists = Playlists.slice(3, 6);
+  const [accessToken, setAccessToken] = useState("");
+  const [albumsData, setAlbumsData] = useState([]);
+
+  useEffect(() => {
+    const getClientCredentialsToken = async () => {
+      try {
+        const clientId = "a5df27b1278d4ca5970f3e3ee528d458";
+        const clientSecret = "0b044f4d73394d97b0cc9adb6933136d";
+
+        const base64Auth = btoa(`${clientId}:${clientSecret}`);
+
+        const response = await axios.post(
+          "https://accounts.spotify.com/api/token",
+          "grant_type=client_credentials",
+          {
+            headers: {
+              Authorization: `Basic ${base64Auth}`,
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+          }
+        );
+
+        setAccessToken(response.data.access_token);
+      } catch (error) {
+        console.error("Ошибка получения токена:", error.response.data);
+      }
+    };
+
+    getClientCredentialsToken();
+  }, []);
+
+  useEffect(() => {
+    const getAlbumsData = async () => {
+      try {
+        if (accessToken) {
+          const albumIds =
+            "382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc";
+
+          const response = await axios.get(
+            `https://api.spotify.com/v1/albums?ids=${albumIds}&limit=20`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+
+          setAlbumsData(response.data.albums);
+        }
+      } catch (error) {
+        console.error("Ошибка получения данных альбомов:", error.response.data);
+      }
+    };
+
+    getAlbumsData();
+  }, [accessToken]);
 
   return (
     <div className="library">
@@ -25,6 +79,25 @@ const Library = () => {
           ))}
         </div>
       </div>
+
+      {albumsData.map((albumData) => (
+        <div key={albumData.id} className="albumData">
+          <img src={albumData.images[0].url} alt={albumData.name} />
+          <p>Album:</p>
+          {albumData.name && <p>Album Name: {albumData.name}</p>}
+          {albumData.artists && (
+            <p>
+              Artist:{" "}
+              {albumData.artists.map((artist) => artist.name).join(", ")}
+            </p>
+          )}
+          {albumData.release_date && (
+            <p>Date of Release: {albumData.release_date}</p>
+          )}
+          {albumData.total_tracks && <p>Songs: {albumData.total_tracks}</p>}
+          {/* Добавьте другие поля альбома по вашему усмотрению */}
+        </div>
+      ))}
     </div>
   );
 };
